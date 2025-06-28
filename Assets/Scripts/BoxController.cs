5d8b1bbdf7561dc036a4feb3fc020810c5725b47
William Ardila <williamardilasanchez@gmail.com>
2025-06-28 17:57:54 -0500

    Rotation of the box fully implemented

--- Assets/Scripts/BoxController.cs
+++ Assets/Scripts/BoxController.cs
@@ -3,20 +3,27 @@ using UnityEngine.InputSystem;
 
 public class BoxController : MonoBehaviour
 {
-    // Variables
+    // Input
     private InputDevice device;
     private PlayerInput playerInput;
     private InputAction rotateAction;
     private InputAction zoomInAction;
     private InputAction zoomOutAction;
 
-    private Vector2 rotationInput;
-    private Vector2 accumulatedInput;
-    [SerializeField] private float rotationSpeed;
-    [SerializeField] private float rotationTreshold;
+    // Rotation movement
     [SerializeField] private float mouseSensitivityFactor;
+    [SerializeField] private float minInputValue;
+    private Vector2 rotationInput;
 
-    private GameObject mainCamera;
+    // Rotation interpolation
+    private Quaternion startRotation;
+    private Quaternion targetRotation;
+    private bool isRotating = false;
+    private float rotationTimer = 0f;
+    [SerializeField] private float rotationDuration;
+
+    // Zoom
+    private Camera mainCamera;
     private float zoomInInput;
     private float zoomOutInput;
     [SerializeField] private float minZoom;
@@ -25,7 +32,9 @@ public class BoxController : MonoBehaviour
 
     void Awake()
     {
-        mainCamera = GameObject.Find("Main Camera");
+        if (mainCamera == null)
+            mainCamera = Camera.main;
+
         playerInput = GetComponent<PlayerInput>();
         rotateAction = playerInput.actions["Rotate"];
         zoomInAction = playerInput.actions["Zoom In"];
@@ -34,11 +43,23 @@ public class BoxController : MonoBehaviour
 
     void Update()
     {
+        if (isRotating)
+        {
+            rotationTimer += Time.deltaTime / rotationDuration;
+            transform.rotation = Quaternion.Lerp(startRotation, targetRotation, rotationTimer);
+
+            if (rotationTimer >= 1f) // End of interpolation
+            {
+                transform.rotation = targetRotation;
+                isRotating = false;
+            }
+
+            return;
+        }
+
+        // Reading inputs
         rotationInput = rotateAction.ReadValue<Vector2>();
-
-        // Detect the last device that sent input
-        device = rotateAction.activeControl?.device;
-
+        device = rotateAction.activeControl?.device; // Detect the last device that sent input
         RotateBox();
 
         zoomInInput = zoomInAction.ReadValue<float>();
@@ -48,90 +69,54 @@ public class BoxController : MonoBehaviour
         ZoomOut();
     }
 
-    // Rotation
+    // Rotation methods
     public void RotateBox()
     {
-        if (rotationInput != Vector2.zero)
+        // Save rotationInput in a new variable to process if a mouse is used
+        Vector2 processedInput = rotationInput;
+        if (device is Mouse)
+        processedInput *= mouseSensitivityFactor; // Scale the mouse delta so that it behaves like a stick
+
+        // Ignore very small inputs to avoid unwanted rotations
+        if (processedInput.magnitude < minInputValue) return;
+
+        Debug.Log(processedInput.magnitude);
+
+        // Validate the dominant axis of rotation
+        float absX = Mathf.Abs(processedInput.x);
+        float absY = Mathf.Abs(processedInput.y);
+
+        if (absX > absY)
         {
-            Vector2 processedInput = rotationInput;
-
-            // Accumulate the value of the input to check when it should rotate
-            accumulatedInput += processedInput;
-
-            // Normalize and scale the mouse delta so that it behaves like a stick
-            if (device is Mouse)
-            {
-                processedInput = rotationInput.normalized * mouseSensitivityFactor;
-            }
-
-            // Validate the dominant axis of rotation
-            float absX = Mathf.Abs(processedInput.x);
-            float absY = Mathf.Abs(processedInput.y);
-
-            //if (absX > absY && Mathf.Abs(accumulatedInput.x) > rotationTreshold)
-            if (absX > absY)
-            {
-                RotateHorizontally(processedInput);
-            }
-            //else if (absY > absX && Mathf.Abs(accumulatedInput.y) > rotationTreshold)
-            else if (absX < absY)
-            {
-                RotateVertically(processedInput);
-            }
+            RotateHorizontally(Mathf.Sign(processedInput.x));
         }
-        else
+        else if (absY > absX)
         {
-            accumulatedInput = Vector2.zero; 
+            RotateVertically(Mathf.Sign(processedInput.y));
         }
     }
 
-    private void RotateHorizontally(Vector2 processedInput)
+    private void RotateHorizontally(float direction)
     {
-        transform.Rotate(Vector3.up, processedInput.x * rotationSpeed * Time.deltaTime);
-
-        /*if (processedInput.x > 0)
-        {
-            transform.Rotate(Vector3.up, 90);
-        }
-        else
-        {
-            transform.Rotate(Vector3.up, -90);
-        }
-
-        accumulatedInput = Vector2.zero; */
+        startRotation = transform.rotation;
+        targetRotation = Quaternion.AngleAxis(90f * direction, Vector3.up) * transform.rotation; // Using the global Y axis
+        BeginLerp();
     }
 
-    private void RotateVertically(Vector2 processedInput)
+    private void RotateVertically(float direction)
     {
-        transform.Rotate(Vector3.right, processedInput.y * rotationSpeed * Time.deltaTime);
-
-        /*if (processedInput.y > 0)
-        {
-            if (Mathf.Abs(transform.rotation.y) == 90)
-            {
-                transform.Rotate(Vector3.forward, 90);
-            }
-            else
-            {
-                transform.Rotate(Vector3.right, 90);
-            }
-        }
-        else
-        {
-            if (Mathf.Abs(transform.rotation.y) == 90)
-            {
-                transform.Rotate(Vector3.forward, -90);
-            }
-            else
-            {
-                transform.Rotate(Vector3.right, -90);
-            }
-        }
-        
-        accumulatedInput = Vector2.zero; */
+        startRotation = transform.rotation;
+        targetRotation = Quaternion.AngleAxis(90f * direction, Vector3.left) * transform.rotation; // Using the global X axis
+        BeginLerp();
     }
 
-    // Zoom
+    private void BeginLerp()
+    {
+        rotationTimer = 0f;
+        isRotating = true;
+    }
+
+    // Zoom methods
     private void ZoomIn()
     {
         if (zoomInInput != 0 && mainCamera.transform.position.z < maxZoom)
